3e38c7eb8c5250481d1cd1675643418735a02c99
Luiz Araújo <luizcaj@yahoo.com.br>
2023-03-06 10:09:49 -0300

    chore: create test examples

---
+++ hooks/useConter.test.ts
@@ -0,0 +1,13 @@
+import { renderHook } from '@testing-library/react';
+import { act } from '@test/testUtils';
+import useCounter from './useCounter';
+
+test('should increment counter', () => {
+  const { result } = renderHook(() => useCounter());
+
+  act(() => {
+    result.current.increment();
+  });
+
+  expect(result.current.count).toBe(1);
+});
